460691d71e95810acbc188f0dbe82e250e1dc03c
Matthew Crenshaw <scubasonar@gmail.com>
2013-04-03 14:42:26 -0700

    started on hardware test software

---
+++ antenduino_hardwareTest.ino
@@ -0,0 +1,19 @@
+/* Pins */
+#define PIN_LIMITB_A = 4; // red, normally open, connects to ground
+#define PIN_LIMITB_B = 5; // blue, normally open, connects to ground
+#define PIN_LIMITA_A = 6; // red, normally open, connects to ground
+#define PIN_LIMITA_B = 7; // blue, normally open, connects to ground
+#define PIN_PWM_A    = 3;  // controls motor A speed
+#define PIN_PWM_B    = 11; // controls motor B speed
+#define PIN_DIR_A    = 12; 
+#define PIN_DIR_B    = 13; 
+
+#define PIN_SENSOR_IR = 14; // analog 0
+
+void setup()
+{
+}
+
+void loop()
+{
+}
